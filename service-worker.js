22485dde9ad03184d98e79d45221bcecf7266d91
mustafa2smi-ui <mustafa2smi@gmail.com>
2025-11-22 10:25:28 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,30 +6,46 @@ const urlsToCache = [
   '/manifest.json',
   '/saral.jpg',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
-];
-
+]
+// Install event
 self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
-  self.skipWaiting();
 });
 
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
 
+// Activate event
 self.addEventListener('activate', event => {
+  console.log('Service Worker activating.');
+  // Remove old caches
   event.waitUntil(
-    caches.keys().then(names => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        names.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
           }
         })
       );
